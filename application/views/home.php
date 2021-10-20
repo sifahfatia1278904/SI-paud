65a7d58f830917a4d2212b2dc0fc53b026cb6804
sifah fatia sobrina <sifahfatiasobrina06@gmail.com>
2021-10-20 12:58:12 +0700

    mmenambahkan code pada home

--- application/views/home.php
+++ application/views/home.php
@@ -8,5 +8,24 @@
 </head>
 <body>
     
-</body>
+</body>n="3"><strong>Form Login</strong></td>
+			</tr>
+			<tr>
+				<td>Username</td>
+				<td>:</td>
+				<td><input name="username" type="text" id="username"/>
+				</td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td>:</td>
+				<td><input name="password" type="password" id="password"/></td>
+			</tr>
+			<tr>
+				<td><input type="submit" name="Submit" value="Login"/></td>
+			</tr>
+		</form>
+		</table>
+	</head>
+</html>
 </html>
